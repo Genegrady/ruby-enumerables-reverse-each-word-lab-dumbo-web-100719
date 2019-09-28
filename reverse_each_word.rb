77a9d71f8d969ef46c246947a3cc5eb4c469785e
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-28 23:32:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  split_string = string.split("")
+  split_string = string.split(" ")
   reversed = []
   split_string.each do |string|
     reversed << string.reverse 
   end
-  reversed.join("")
+  reversed.join(" ")
 end
