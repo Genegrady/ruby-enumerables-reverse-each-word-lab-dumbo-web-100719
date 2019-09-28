6639c60aa947aa983b24b28852b4959b15d79b50
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-28 23:30:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
-  string.size{ reversed << split_string.pop }
+  string.size.times{ reversed << split_string.pop }
   reversed.join("")
 end
